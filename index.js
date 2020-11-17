3c12c89e17810a32ee23e2483a3c4c2764b7bf60
ScienceNibba <tommy.ornato@gmail.com>
2020-11-17 18:51:29 +0100

    here, catch this try and catch. dumbass

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.login('YOYO') //TODO save the key in a decent and secure way
+try {
+    const Discord = require('discord.js');
+    const client = new Discord.Client();
+    client.login('YOYO') //TODO save the key in a decent and secure way
 
-const channel = 'YUYU';
-const donne = ['YEYE'];
+    const channel = 'YUYU';
+    const donne = ['YEYE'];
 
-client.on('ready', () => {
-    console.log('Logged in');
-});
+    client.on('ready', () => {
+        console.log('Logged in');
+    });
 
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-    const newUserChannel = newMember.channelID;
-    const oldUserChannel = oldMember.channelID;
-    const textChannel = newMember.guild.channels.cache.find(channel => channel.name === 'botteria'); // Select spam channel
-    const User = client.users.cache.get(newMember.id); // Getting user by ID
+    client.on('voiceStateUpdate', (oldMember, newMember) => {
+        const newUserChannel = newMember.channelID;
+        const oldUserChannel = oldMember.channelID;
+        const textChannel = newMember.guild.channels.cache.find(channel => channel.name === 'botteria'); // Select spam channel
+        const User = client.users.cache.get(newMember.id); // Getting user by ID
 
-    if (donne.includes(User.id)) { // Checking if the user exists
-        
-        if (newUserChannel === channel) {  //If voice channel is...
-            textChannel.send(`${User} Ciao Marta!`);    //THAT?S IT
-            console.log(`Joined ${User}`)   //Debug
-        } else if (oldUserChannel === channel && newUserChannel !== channel) {    //If client left voice channel. Not really useful, might remove
-            console.log(`Left ${User}`)
+        if (donne.includes(User.id)) { // Checking if the user exists
+
+            if (newUserChannel === channel) {  //If voice channel is...
+                textChannel.send(`${User} Ciao Marta!`);    //THAT?S IT
+                console.log(`Joined ${User}`)   //Debug
+            } else if (oldUserChannel === channel && newUserChannel !== channel) {    //If client left voice channel. Not really useful, might remove
+                console.log(`Left ${User}`)
+            }
         }
-    }
 
-})
+    })
+}
+
+catch(err){
+    console.log(err)//It won't happen. But if it happened... No actually heroku and repl are bothering me with this shit. If it were me If I had any choiche I'd never lose time with this crap. Fuck this crap
+}
